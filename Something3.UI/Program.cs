f2776c815a93a6c2169f71b828bb202aedfe6ce0
Stephen Green <SGreen@qicglobal.com>
2020-08-28 09:09:31 +0100

    Tidied up.

--- Something3.UI/Program.cs
+++ Something3.UI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Something3UI
+namespace Something3.UI
 {
     public class Program
     {
